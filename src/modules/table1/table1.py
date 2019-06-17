cc2566912b668a91fc5da6fed7e1dba7ab8b097f
Jing wen <jingwen.saw@holmusk.com>
2019-06-17 12:10:38 +0800

    test

--- src/modules/table1/table1.py
+++ src/modules/table1/table1.py
@@ -48,9 +48,9 @@ def fetchTable1MD(logger, df):
         tableString += "### Overview\n"
         for item in ['race','age','sex']: #,'visit_type'
             out = df[item].value_counts().compute().to_frame()
-            tableString += tabulate(out.transpose(), tablefmt="pipe", headers="keys") #\
-                                                                                    #.replace("age", "").replace("race", "").replace("sex", "") #REMOVE HEADINGS
-            #tableString += "\n"
+            tableString += tabulate(out.transpose(), tablefmt="pipe", headers="keys") \
+                                                                                    .replace("age", " ").replace("race", " ").replace("sex", " ") #REMOVE HEADINGS
+            tableString += "\n"
 
     except Exception as e:
         logger.error(f'Issue with printing column "All": " {e}')
